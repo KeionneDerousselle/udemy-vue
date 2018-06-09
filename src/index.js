b30d0a504d5f9aca3c7355873b0013598c725558
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-09 12:04:19 -0600

    computed vs watch. computed is more optimized but you can also watch data properties

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ new Vue({
   el: '#app',
   data: {
     counter: 0,
-    secondCounter: 0
+    secondCounter: 0,
+    watchResult: ''
   },
   computed: {
     output: function() {
-      console.log('Computed executed')
+      console.log('Computed executed');
       return this.counter > 5 ? 'greater than 5' : 'less than 5';
+    }
+  },
+
+  watch: {
+    counter: function(value) {
+      console.log('Watch executed');
+      this.watchResult = value > 5 ? 'greater than 5' : 'less than 5';
     }
   },
 
