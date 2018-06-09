e36a9e42e9d6aef94144a032e1b1dd2d8d5c3dd7
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-09 13:29:36 -0600

    use : as a shorthand for the v-bind directive

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0,
-    secondCounter: 0,
-    watchResult: ''
-  },
-  computed: {
-    output: function() {
-      console.log('Computed executed');
-      // computed runs synchronously and is expected to return something
-      return this.counter > 5 ? 'greater than 5' : 'less than 5';
-    }
-  },
-
-  watch: {
-    counter: function(value) {
-      console.log('Watch executed');
-      // watch can run asynchronous code blocks because it does not return a value
-      // and therefore can retrieve something from the server
-      var vm = this;
-      setTimeout(function () {
-        vm.counter = 0;
-      }, 2000)
-      this.watchResult = value > 5 ? 'greater than 5' : 'less than 5';
-    }
+    link: 'http://google.com',
   },
 
   methods: {
-    result: function() {
-      console.log('Method executed');
-      return this.counter > 5 ? 'greater than 5' : 'less than 5';
+    changeLink: function() {
+      this.link = 'http://apple.com'
     }
   }
 });
