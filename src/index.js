81698d39c917a20b68d3cb0ecab42560f1788c43
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-09 11:01:41 -0600

    on click event handler

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     title: 'Hello, World!',
     link: 'http://google.com',
-    finishedLink: '<a href="https://google.com">Google</a>'
+    finishedLink: '<a href="https://google.com">Google</a>',
+    counter: 0
   },
   methods: {
     changeTitle: function(event) {
@@ -13,6 +14,10 @@ new Vue({
     sayHello: function() {
       this.title = 'Something Else'
       return this.title;
+    },
+
+    increase: function() {
+      this.counter++;
     }
   }
 });
