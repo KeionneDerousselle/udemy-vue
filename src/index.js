13934cdc08a16102c5dddd19da9e60ad10cfdfba
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-09 11:56:52 -0600

    computed properties vs methods. methods get called every time because vue does not know which data properties it uses

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello, World!',
-    link: 'http://google.com',
-    finishedLink: '<a href="https://google.com">Google</a>',
     counter: 0,
-    x: 0,
-    y: 0,
-    name: 'Keionne'
+    secondCounter: 0
   },
+  computed: {
+    output: function() {
+      console.log('Computed executed')
+      return this.counter > 5 ? 'greater than 5' : 'less than 5';
+    }
+  },
+
   methods: {
-    changeTitle: function(event) {
-      this.title = event.target.value;
-    },
-
-    sayHello: function() {
-      this.title = 'Something Else'
-      return this.title;
-    },
-
-    increase: function(step, event) {
-      this.counter += step;
-    },
-
-    updateCoordinates: function(event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-
-    alertMe: function() {
-      alert('Alert!')
+    result: function() {
+      console.log('Method executed');
+      return this.counter > 5 ? 'greater than 5' : 'less than 5';
     }
   }
 });
