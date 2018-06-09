64ac4f0c8d6b85f0a888228e166f46d1326fbfc6
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-09 15:01:59 -0600

    conditional rendering with the v-if directive

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 new Vue({
   el: '#app',
   data: {
-    color: 'green',
-    width: 100
-  },
-  
-  computed: {
-    myStyle: function() {
-      return {
-        backgroundColor: this.color,
-        width: this.width + 'px'
-      }
-    }
+    show: true
   }
 });
