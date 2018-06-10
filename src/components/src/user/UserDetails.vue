468cdd8b68fbe7fe733ed8053234ae4c6dc07eec
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-10 17:59:46 -0600

    Sending data to a parent component via emmiting an event

--- src/components/src/user/UserDetails.vue
+++ src/components/src/user/UserDetails.vue
@@ -3,6 +3,7 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User Name: {{ switchName() }}</p>
+    <button @click="resetName">Reset Name</button>
   </div>
 </template>
 
@@ -10,17 +11,18 @@
   export default {
     props: {
       name: {
-        type: Object,
-        default: function() {
-          return {
-            name: 'Keionne'
-          }
-        }
+        type: String,
+        required: true
       }
     },
     methods: {
       switchName() {
         return this.name.split('').reverse().join('');
+      },
+
+      resetName() {
+        this.name = 'Keionne';
+        this.$emit('nameWasReset', this.name);
       }
     }
   }
