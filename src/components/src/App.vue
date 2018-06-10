e113ec99856118387b9c926093420d5f013e8711
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-10 17:29:52 -0600

    Passing data from a parent to child using a bind directive and props

--- src/components/src/App.vue
+++ src/components/src/App.vue
@@ -1,31 +1,26 @@
 <template>
-  <div>
-    <app-header></app-header>
-    <div class="container">
-      <div class="row">
-        <app-servers></app-servers>
-        <app-server-details></app-server-details>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-user></app-user>
       </div>
     </div>
-    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-  import Header from './common/Header.vue';
-  import Footer from './common/Footer.vue';
-  import Servers from './servers/Servers.vue';
-  import ServerDetails from './servers/ServerDetails.vue';
+  import User from "./user/User.vue";
 
   export default {
     components: {
-      'app-header': Header,
-      'app-servers': Servers,
-      'app-server-details': ServerDetails,
-      'app-footer': Footer
+      appUser: User
     }
   };
 </script>
 
 <style>
+  div.component {
+    border: 1px solid black;
+    padding: 30px;
+  }
 </style>
---
+++ src/components/src/user/User.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-details :name="name"></app-user-details>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import UserDetails from "./UserDetails.vue";
+  import UserEdit from "./UserEdit.vue";
+
+  export default {
+    data: function() {
+      return {
+        name: 'Keionne'
+      };
+    },
+
+    methods: {
+      changeName(){
+        this.name = 'Kei'
+      }
+    },
+
+    components: {
+      appUserDetails: UserDetails,
+      appUserEdit: UserEdit
+    }
+  };
+</script>
+
+<style>
+</style>
