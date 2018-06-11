2de30c936e1921332ea0d93d19d7be22db2b1269
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-10 21:51:45 -0600

    components that are generated dynamically are destroyed by default

--- src/advanced-components/src/components/New.vue
+++ src/advanced-components/src/components/New.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <h3>New</h3>
+    <button @click="counter++">Increment the Counter</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 
 <script>
-  export default {};
+  export default {
+    data: function() {
+      return {
+        counter: 0
+      };
+    }
+  };
 </script>
 
 <style scoped>
