b06560bf6ebb0a8725b4398ea7a444f5988f9ba2
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-10 21:46:52 -0600

    dynamically changing content using the component tag

--- src/advanced-components/src/App.vue
+++ src/advanced-components/src/App.vue
@@ -2,10 +2,15 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <app-quote>
+        <button @click="selectedComponent = 'app-quote'">Quote</button>
+        <button @click="selectedComponent = 'app-author'">Author</button>
+        <button @click="selectedComponent = 'app-new'">New</button>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+        <component :is="selectedComponent">
           <h2 slot="title">{{ quoteTitle }}</h2>
           <p>A wonderful Quote!</p>
-        </app-quote>
+        </component>
       </div>
     </div>
   </div>
@@ -13,15 +18,20 @@
 
 <script>
   import Quote from "./components/Quote.vue";
+  import Author from "./components/Author.vue";
+  import New from "./components/New.vue";
 
   export default {
     data: function() {
       return {
-        quoteTitle: 'The Quote'
+        quoteTitle: 'The Quote',
+        selectedComponent: 'app-quote'
       };
     },
     components: {
-      "app-quote": Quote
+      'app-quote': Quote,
+      'app-author':Author,
+      'app-new': New
     }
   };
 </script>
