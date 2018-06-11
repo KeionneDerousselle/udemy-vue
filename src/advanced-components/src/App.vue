0a2867d350d631b6a850b868909aaca9ded0deba
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-10 21:27:44 -0600

    adding content to a child component using slots

--- src/advanced-components/src/App.vue
+++ src/advanced-components/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div id="app">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <app-quote>
+          <h2>{{ quoteTitle }}</h2>
+          <p>A wonderful Quote!</p>
+        </app-quote>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Quote from "./components/Quote.vue";
+
+  export default {
+    data: function() {
+      return {
+        quoteTitle: 'The Quote'
+      };
+    },
+    components: {
+      "app-quote": Quote
+    }
+  };
 </script>
 
 <style>
