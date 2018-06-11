c35851561bee4909cbdced7ab96a5abb7cc00e53
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-10 21:52:48 -0600

    wrap dynamic components in keep-alive tag to keep them alive

--- src/advanced-components/src/App.vue
+++ src/advanced-components/src/App.vue
@@ -7,10 +7,12 @@
         <button @click="selectedComponent = 'app-new'">New</button>
         <hr>
         <p>{{ selectedComponent }}</p>
-        <component :is="selectedComponent">
-          <h2 slot="title">{{ quoteTitle }}</h2>
-          <p>A wonderful Quote!</p>
-        </component>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <h2 slot="title">{{ quoteTitle }}</h2>
+            <p>A wonderful Quote!</p>
+          </component>
+        </keep-alive>
       </div>
     </div>
   </div>
@@ -24,14 +26,14 @@
   export default {
     data: function() {
       return {
-        quoteTitle: 'The Quote',
-        selectedComponent: 'app-quote'
+        quoteTitle: "The Quote",
+        selectedComponent: "app-quote"
       };
     },
     components: {
-      'app-quote': Quote,
-      'app-author':Author,
-      'app-new': New
+      "app-quote": Quote,
+      "app-author": Author,
+      "app-new": New
     }
   };
 </script>
