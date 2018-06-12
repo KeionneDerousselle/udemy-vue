5185ac4c649098dfaa51ceaa81d3aacd7208671a
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-11 21:11:22 -0600

    setting up vue router

--- src/routing/src/App.vue
+++ src/routing/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 ofset-sm-2 col-md-6 offset-md-3">
+        <h1>Routing</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
